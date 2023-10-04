7e1cf8bfb46739bfb13d8e6e29ace8b5e49c01e7
astvu29429422u18019 <144683766+astvu29429422u18019@users.noreply.github.com>
2023-10-04 14:29:13 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,22 @@
+
+class player:
+  def play(self):
+    print("The Player is Playing Cricket.")
+
+
+class Batsman(player):
+  def play(self):
+    print ("The Batsman is Batting.")
+
+
+class Bowler(player):
+  def play(self):
+    print("The Bowler is Bowling:")
+
+
+batsman=Batsman()
+bowler=Bowler()
+
+
+batsman.play()
+bowler.play()
